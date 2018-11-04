c1e3a5b231eecace565442e6641230fe909697c8
Julián Melero Hidalgo <jmelero@julian>
2018-11-04 20:51:25 +0100

    Cambios en clases y alta_usuario

--- alta_usuario.php
+++ alta_usuario.php
@@ -5,7 +5,6 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
 
 require_once "class/tipos_usuarios.php";
 $tipos = new tipo_usuario();
-
 ?>
 
 <!DOCTYPE html>
@@ -15,12 +14,18 @@ $tipos = new tipo_usuario();
     <title>OMEGA NEWS - Nuevo Usuario</title>
   </head>
   <body>
-    <form class="" action="index.html" method="post">
+    <form class="" action="" method="post">
+      <label for="nombre">Usuario*</label>
       <input type="text" required name="nombre">
+      <label for="apellido1">Primer Apellido*</label>
       <input type="text" required name="apellido1">
+      <label for="apellido2">Segundo Apellido*</label>
       <input type="text" required name="apellido2">
-      <input type="text" required name="email">
-      <input type="text" required name="telefono">    
+      <label for="email">Email*</label>
+      <input type="email" required name="email">
+      <label for="telefono">Teléfono*</label>
+      <input type="text" required name="telefono">
+      <input type="submit" value="Registrarme">
     </form>
   </body>
 </html>
--- class/usuarios.php
+++ class/usuarios.php
@@ -12,17 +12,29 @@ class usuario{
 
     }
 
-    function existe($usuario,$pwd){
+    function autenticacion($usuario,$pwd){
 
     }
 
+    function existe($usuario){
 
-    function set_usuario($usuario, $pass, $ape1, $ape2,$email, $telefono){
+    }
+    function set_usuario($usuario, $pass, $ape1, $ape2,$email, $telefono){      
+      $tipos = new tipo_usuario();
+      $errores = 0;
       // Cuando se da de alta siempre será periodista
       $datos = $tipos->get_id_periodista();
       while($t_usuarios = $datos[0]->fetch()){
         $id_perdista = $t_usuarios["id"];
       }
+      if (isnull($id_perdista)) {
+        $errores +=1;
+      }
+
+      if ($errores>0) {
+        die("Lo sentimos :( No se pudo dar de alta, contacte con Omega News");
+      }
+
     }
 
 
