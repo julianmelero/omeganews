7588038ff31807e5ca9ea468295cde66b5577651
Julián Melero <julian@melerohidalgo.es>
2018-11-07 18:55:45 +0100

    Autenticación y registro de usuario corregido.

--- header.php
+++ header.php
@@ -5,13 +5,17 @@
 Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Oleagagoitia Álvarez
 */
 
-// Vamos a ver si existe una sesión con el usuario, en caso contratio lo mandamos a autenticar
+// Vamos a ver si existe una sesión con el usuario para ver si tiene que cerrar sesión
 session_start();
 if(! isset($_SESSION["usuario"])){
-  header('Location: autenticacion.php');
+?>
+¡Bienvenido a OMEGA NEWS! <a href="autenticacion.php">Autentifícate</a> o <a href="alta_usuario.php">Regístrate.</a>
+<?php
 }
-
-
+else{
+?>
+¡Hola <?php echo $_SESSION["usuario"] ?>! <a href="cerrar_sesion.php">Cerrar sesión</a>
+<?php
+}
 ?>
 
-¡Hola <?php echo $_SESSION["usuario"] ?>! <a href="cerrar_sesion.php">Cerrar sesión</a>
