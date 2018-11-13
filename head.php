67c5c229d6950b407bc3d9f8c84c4a77a89e89b0
Julián Melero <julian@melerohidalgo.es>
2018-11-13 17:18:27 +0100

    Añado fuente brandon y cambio css

--- head.php
+++ head.php
@@ -1,2 +1,3 @@
 <link rel="stylesheet" href="./css/estilos.css">
 <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<link href="./sources/fonts/Brandon_bld.otf" rel="stylesheet">
