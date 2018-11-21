129960507a2a27744e6c72e0b9d442054f05b709
Julián Melero <julian@melerohidalgo.es>
2018-11-21 13:35:34 +0100

    Formulario publicar noticia

--- publicar_noticias.php
+++ publicar_noticias.php
@@ -9,5 +9,28 @@
 </head>
 <body>
 <?php require_once "header.php"; ?> 
+
+<form action="" method="post">
+    <input type="hidden" name="id_usuario" value='<?php  ?>'>
+    <label for="titulo">Título</label>
+    <input type="text" name="titulo" required maxlenght="250">
+    <label for="subtitulo">Subtítulo</label>
+    <input type="text" name="subtitulo" required maxlenght="250">
+    <label for="fecha">Fecha</label>
+    <input type="date" name="fecha" required>
+    <label for="texto_noticia">Fecha</label>
+    <textarea name="texto_noticia" id="" cols="30" rows="10" required></textarea>
+    <label for="url_img">Imagen</label>
+    <input type="file" name="url_img" id="url_img" accept="image/png, image/jpeg">
+    <input type="submit" value="Guardar noticia">
+    
+    
+
+
+
+</form>
+
+
+
 </body>
 </html>
