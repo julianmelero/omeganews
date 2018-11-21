b6d387ce985e2bb93d2b6420cb8fac5683dd4263
Julián Melero <julian@melerohidalgo.es>
2018-11-21 11:35:33 +0100

    En miembros pongo el nombre del tipo de usuario

--- class/tipos_usuarios.php
+++ class/tipos_usuarios.php
@@ -14,6 +14,15 @@ class tipo_usuario{
     return $resultado =$con->query($query, array());
   }
 
+  function get_tipo_usuario($id){
+    $con = new conexion();
+    $query = "SELECT nombre_tipo FROM tipos_usuario WHERE id = ?;";
+    $resultado =$con->query($query, array($id));
+    while($usuarios = $resultado[0]->fetch()){
+      return $usuarios["nombre_tipo"];
+    }
+  }
+
   function get_id_periodista(){
     $con = new conexion();
     $query = "SELECT * FROM tipos_usuario WHERE nombre_tipo='Periodista';";
--- miembros.php
+++ miembros.php
@@ -27,19 +27,37 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
   	}
 	require_once "class/usuarios.php";
 	$usuario = new usuario();
+	$tipos = new tipo_usuario();
 	$datos = $usuario->get_usuarios();
+	
+
+
+
+
+
+
+
+
+
+
+
+	// Recorremos los datos de los usuarios
 	while($usuarios = $datos[0]->fetch()){		
+		//Cogemos el tipo de usuario
+		$nombre_tipo = $tipos->get_tipo_usuario($usuarios["id_tipo_usuario"]);
+
 	?>   
 		<form action="" method="post">
     
 			<div class="miembros">					
 				<tr>
 					<td><?php echo $usuarios["nombre"]." ".$usuarios["ape1"]." ".$usuarios["ape2"];  ?></td>
-					<td><?php echo $usuarios["id_tipo_usuario"];  ?></td>
+					<td><?php echo $nombre_tipo;  ?></td>
 					<td>
 					<input type="submit" value="Convertir a Periodista">	
 				<input type="submit" value="Convertir a Editor">
-				<input type="submit" value="Convertir a Administrador">				
+				<input type="submit" value="Convertir a Administrador">
+				<hr>				
 				</td>
 				</tr>
 			
