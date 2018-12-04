ec6d48ceeed177062edc6a16d8748ce6fb720524
Julián Melero <julian@melerohidalgo.es>
2018-12-04 13:58:06 +0100

    Accesibilidad

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
           }      
           echo "<input type='hidden' name='id' value='".$datos["id"]."' id='id'></input> ";
           echo "<h2>".$datos["titulo"]."</h2>";
-          echo "<img src='".$dir."'  class='imagen'>";
+          echo "<img src='".$dir."'  class='imagen' alr='imagen'>";
           echo "<h3>".$datos["subtitulo"]."</h3>";
           echo "<h4>".date("d-m-Y",strtotime($datos["fecha"]))."</h4>";
           echo substr($datos["texto_noticia"],0,150);
