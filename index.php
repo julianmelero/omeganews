18bd85b3339d80cbc33afa619216a24643bcb7db
agarridogar <35024653+agarridogar@users.noreply.github.com>
2018-12-05 23:47:41 +0100

    Búsqueda  tipo de sección
    Búsqueda de noticias por tipo de sección con un select

--- index.php
+++ index.php
@@ -16,28 +16,7 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
     <?php require_once "header.php"; 
         require_once "class/secciones.php";
         $secciones = new secciones();
-    ?>
         
-    
-   <!--select para elegir las secciones de noticias-->
-    <form enctype="multipart/form-data" action="publicar_noticias.php" method="post">
-        <select name="id_seccion" id="seccion">
-        <option>Todas</option>
-    <?php  
-    $datos = $secciones->get_secciones();
-   
-    while($seccion = $datos[0]->fetch()){        
-        ?>        
-        <option value="<?php echo $seccion["id"]; ?>" ><?php echo $seccion["nombre"]; ?></option>        
-        <?php
-    }
-
-    ?>
-    </select>
-    </form>
-
-
-    <?php
       // Primero mostramos las noticias que faltan por revisar
       require_once "class/publicacion.php";
       require_once "class/usuarios.php";
@@ -71,7 +50,37 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
       }
     }
 
-    // Las noticias
+    ?>
+      
+    <!--select para elegir las secciones de noticias-->
+    <div name="filtros">
+        <form enctype="multipart/form-data" action="index.php" method="post">
+        <select name="filtro" id="seccion">
+                <option value="Todas">Todas</option>
+             <?php  
+            $datos = $secciones->get_secciones();
+
+            while($seccion = $datos[0]->fetch()){        
+                ?>        
+                <option value="<?php echo $seccion["id"]; ?>" ><?php echo $seccion["nombre"]; ?></option>        
+                <?php
+            }
+
+            ?>
+        </select>
+            <button type="submit">Filtrar</button>
+
+       </form>
+    </div>
+    
+    <?php
+   
+    if(isset($_POST['filtro'])){
+        
+        switch($_POST['filtro']){
+        case "Todas":
+        
+     // Las noticias aprobadas sin filtrar
       $aprobadas = $publicaciones->get_publicaciones();
       
         echo "<h1>Noticias</h1>";
@@ -82,7 +91,7 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
           $d_usuario = $usuarios->get_usuario($datos["id_usuario"]);
           while ($usuario = $d_usuario[0]->fetch()) {
             echo "Autor: ". $usuario["nombre"]." ".$usuario["ape1"]." ".$usuario["ape2"];
-          }        
+        }
           echo "<h2>".$datos["titulo"]."</h2>";
           echo "<h3>".$datos["subtitulo"]."</h3>";
           echo "<img class='imagen' alt='noticia' src='".$dir."'>";
@@ -97,9 +106,65 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
         }
     
      
+        break;
+        default:
         
+         //Noticias filtradas por sección  
+            $filtradas = $publicaciones->tipo_seccion($_POST['filtro']);
+      
+        echo "<h1>Noticias</h1>";
+        while ($datos = $filtradas[0]->fetch()) {
+          $dir = "img_noticias/".$datos["id"]."/".$datos["url_img"];
+          echo "<form action='noticia.php' method='post'>";
+          echo "<input type='hidden' name='id' value='".$datos["id"]."' id='id'></input> ";
+          $d_usuario = $usuarios->get_usuario($datos["id_usuario"]);
+          while ($usuario = $d_usuario[0]->fetch()) {
+            echo "Autor: ". $usuario["nombre"]." ".$usuario["ape1"]." ".$usuario["ape2"];
+        }
+          echo "<h2>".$datos["titulo"]."</h2>";
+          echo "<h3>".$datos["subtitulo"]."</h3>";
+          echo "<img class='imagen' alt='noticia' src='".$dir."'>";
+          echo "<h4>".date("d-m-Y",strtotime($datos["fecha"]))."</h4>";
+          echo substr($datos["texto_noticia"],0,150);
+          if (strlen($datos["texto_noticia"])> 150 ) {
+            echo "...";
+          } 
+          echo "<br>";         
+          echo "<input type='submit' value='Ver' name='ver'>";
+          echo "</form>";
+        }
+        break;
+        }
+        }else{
         
-    
+      // Las noticias aprobadas sin filtrar
+      $aprobadas = $publicaciones->get_publicaciones();
+      
+        echo "<h1>Noticias</h1>";
+        while ($datos = $aprobadas[0]->fetch()) {
+          $dir = "img_noticias/".$datos["id"]."/".$datos["url_img"];
+          echo "<form action='noticia.php' method='post'>";
+          echo "<input type='hidden' name='id' value='".$datos["id"]."' id='id'></input> ";
+          $d_usuario = $usuarios->get_usuario($datos["id_usuario"]);
+          while ($usuario = $d_usuario[0]->fetch()) {
+            echo "Autor: ". $usuario["nombre"]." ".$usuario["ape1"]." ".$usuario["ape2"];
+        }
+          echo "<h2>".$datos["titulo"]."</h2>";
+          echo "<h3>".$datos["subtitulo"]."</h3>";
+          echo "<img class='imagen' alt='noticia' src='".$dir."'>";
+          echo "<h4>".date("d-m-Y",strtotime($datos["fecha"]))."</h4>";
+          echo substr($datos["texto_noticia"],0,150);
+          if (strlen($datos["texto_noticia"])> 150 ) {
+            echo "...";
+          } 
+          echo "<br>";         
+          echo "<input type='submit' value='Ver' name='ver'>";
+          echo "</form>";
+        }
+        }
+        ?>       
+
+    <?php
       // Aquí ponemos las noticias ya aprobadas, y que pueden ser modificadas sólo por admin y editor
 
 
