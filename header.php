4ad126e88b3d0d0d78e73bc6b19fce9aaf471ab1
Julián Melero <julian@melerohidalgo.es>
2018-12-13 11:18:48 +0100

    indentacion header

--- header.php
+++ header.php
@@ -14,32 +14,32 @@ require_once "./class/usuarios.php";
   <div class="center-contents">
     <div class="logo"> <img src="./sources/img/OmegaNews.jpg" alt="omega"></div>
     <div class="center-contents">
-          <nav class="menu-nav">
-            <ul>                  
-                    <li><a href="index.php">Noticias</a></li>
-                    <?php   if(!isset($_SESSION["usuario"])){  ?>
-                    <li><a href="alta_usuario.php">Registro</a></li>
-                    <li><a href="autenticacion.php">Login</a></li>
-                    <li><a href="nosotros.php">Nosotros</a></li>
-                    <?php } ?>
-                    <?php if(isset($_SESSION["usuario"])){
-                      $usuario = new usuario();
-                      $tipo = $usuario->tipo_usuario($_SESSION["usuario"]);
-                      if ($tipo == "Administrador"){                      
-                      ?>                    
-                        <li><a href="miembros.php">Miembros</a></li>
-                      <?php } 
-                      if ($tipo == "Periodista" or $tipo == "Administrador") {                      
-                        ?>
-                        <li><a href="publicar_noticias.php">Publicar noticia</a></li>                                                
-                      <?php
-                      }
-                      ?>
-                        <li><a href="mis_datos.php"><?php echo $_SESSION["usuario"]; ?> (<?php echo $_SESSION["tipo_usuario"]; ?>) </a></li>                        
-                        <li><a href="cerrar_sesion.php">Cerrar Sesión</a></li>
-                    <?php } ?>
-            </ul>
-          </nav>
+      <nav class="menu-nav">
+        <ul>                  
+          <li><a href="index.php">Noticias</a></li>
+          <?php   if(!isset($_SESSION["usuario"])){  ?>
+          <li><a href="alta_usuario.php">Registro</a></li>
+          <li><a href="autenticacion.php">Login</a></li>
+          <li><a href="nosotros.php">Nosotros</a></li>
+          <?php } ?>
+          <?php if(isset($_SESSION["usuario"])){
+            $usuario = new usuario();
+            $tipo = $usuario->tipo_usuario($_SESSION["usuario"]);
+            if ($tipo == "Administrador"){                      
+            ?>                    
+              <li><a href="miembros.php">Miembros</a></li>
+            <?php } 
+            if ($tipo == "Periodista" or $tipo == "Administrador") {                      
+              ?>
+              <li><a href="publicar_noticias.php">Publicar noticia</a></li>                                                
+            <?php
+            }
+            ?>
+              <li><a href="mis_datos.php"><?php echo $_SESSION["usuario"]; ?> (<?php echo $_SESSION["tipo_usuario"]; ?>) </a></li>                        
+              <li><a href="cerrar_sesion.php">Cerrar Sesión</a></li>
+          <?php } ?>
+        </ul>
+      </nav>
     </div>
   </div>
 </header>
