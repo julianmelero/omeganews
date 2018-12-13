c2a9a119a2495caf794d4797c287aade6bfc1a88
Julián Melero <julian@melerohidalgo.es>
2018-12-13 13:29:26 +0100

    longdesc dir del sitio

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
           }      
           echo "<input type='hidden' name='id' value='".$datos["id"]."' id='id'>";
           echo "<h2>".$datos["titulo"]."</h2>";
-          echo "<img src='".$dir."'  class='imagen' alr='imagen' longdesc='imagen'>";
+          echo "<img src='".$dir."'  class='imagen' alr='imagen' longdesc='."echo "www.omega.melerohidalgo.es"; ."'>";
           echo "<h3>".$datos["subtitulo"]."</h3>";
           echo "<h4>".date("d-m-Y",strtotime($datos["fecha"]))."</h4>";
           echo substr($datos["texto_noticia"],0,150);
@@ -120,7 +120,7 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
         }
           echo "<h2>".$datos["titulo"]."</h2>";
           echo "<h3>".$datos["subtitulo"]."</h3>";
-          echo "<img class='imagen' alt='noticia' src='".$dir."' longdesc='imagen'>";
+          echo "<img class='imagen' alt='noticia' src='".$dir."' longdesc='."echo "www.omega.melerohidalgo.es"; ."'>";
           echo "<h4>".date("d-m-Y",strtotime($datos["fecha"]))."</h4>";
           echo substr($datos["texto_noticia"],0,150);
           if (strlen($datos["texto_noticia"])> 150 ) {
@@ -148,7 +148,7 @@ Copyright (C) 2018  Julián Melero Hidalgo, Araceli Garrido García, Alfredo Ole
         }
           echo "<h2>".$datos["titulo"]."</h2>";
           echo "<h3>".$datos["subtitulo"]."</h3>";
-          echo "<img class='imagen' alt='noticia' src='".$dir."' longdesc='imagen'>";
+          echo "<img class='imagen' alt='noticia' src='".$dir."' longdesc='."echo "www.omega.melerohidalgo.es"; ."'>";
           echo "<h4>".date("d-m-Y",strtotime($datos["fecha"]))."</h4>";
           echo substr($datos["texto_noticia"],0,150);
           if (strlen($datos["texto_noticia"])> 150 ) {
